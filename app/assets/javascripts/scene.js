5bfbf85e5604e349f22ec54fe33af640e7f0dba5
Paulo Ancheta <paulo.ancheta@gmail.com>
2015-05-14 23:26:36 -0700

    figured out length difference and plane controls

--- app/assets/javascripts/scene.js
+++ app/assets/javascripts/scene.js
@@ -1,14 +1,16 @@
 $(document).ready(function(){
-  var world, timeStep=1/60, scene, renderer, camera,
-      icosahedronBody, sphereShape, groundShape,
+  var world, scene, renderer, camera,
+      icosahedron, icosahedronBody, sphereShape,
       ground, groundBody, groundShape;
-  // CONSTANTS
-  var GRID_HELPER_SIZE = 40,
-      GRID_HELPER_STEP = 2,
-      FLOOR_MASS       = 0,
-      MASS             = 10,
-      FLOOR_LENGTH     = 75,
-      CANNON_FLOOR_LENGTH = FLOOR_LENGTH - 27;
+
+  // THREE AND CANNON CONSTANTS
+  var TIME_STEP           = 1/60,
+      GRID_HELPER_SIZE    = 40,
+      GRID_HELPER_STEP    = 2,
+      FLOOR_MASS          = 0,
+      MASS                = 1,
+      FLOOR_LENGTH        = 125,
+      CANNON_FLOOR_LENGTH = (FLOOR_LENGTH / 13) * 6;
 
   initThree();
   initCannon();
@@ -17,16 +19,14 @@ $(document).ready(function(){
   function initCannon() {
     world                   = new CANNON.World();
     world.broadphase        = new CANNON.NaiveBroadphase();
+    groundShape             = new CANNON.Box(new CANNON.Vec3(CANNON_FLOOR_LENGTH, CANNON_FLOOR_LENGTH, 1));
     sphereShape             = new CANNON.Sphere(10);
-    groundShape             = new CANNON.Box(new CANNON.Vec3(CANNON_FLOOR_LENGTH - 15, CANNON_FLOOR_LENGTH - 15, 1));
-
     icosahedronBody         = new CANNON.Body({
                                     mass: MASS
                                   });
     groundBody              = new CANNON.Body({
                                     mass: FLOOR_MASS
                                   });
-
     world.solver.iterations = 10;
     world.gravity.set(0,-40,0);
     world.defaultContactMaterial.contactEquationStiffness = 1e9;
@@ -34,15 +34,14 @@ $(document).ready(function(){
 
     icosahedronBody.addShape(sphereShape);
     icosahedronBody.position.set(0,20,0)
-    icosahedronBody.linearDamping     = 0.5;
+    icosahedronBody.linearDamping = 0.5;
     world.addBody(icosahedronBody);
     
-
     groundBody.addShape(groundShape);
-    groundBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1,0.3,0.5),-Math.PI/2 );
+    groundBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1,0,0),-Math.PI/2 );
     world.addBody(groundBody);
 
-    var ballContact         = new CANNON.ContactMaterial( groundBody, icosahedronBody, 0.0, 0.3);
+    var ballContact         = new CANNON.ContactMaterial( groundBody, icosahedronBody, { friction: 0.0, restitution: 0.9 } );
     
     world.addContactMaterial(ballContact);
   }
@@ -60,19 +59,21 @@ $(document).ready(function(){
     renderer.setClearColor( 0x757575 );
     renderer.shadowMapEnabled = true;
     document.body.appendChild( renderer.domElement );
+
     camera.position.set(1,25,100); // camera position to x , y , z
     camera.lookAt( new THREE.Vector3() )
 
+    // SHADOW
     directionalLight.position.set(  40, 50, 20 );
     directionalLight.target.position.set( 0, 0, 0 );
-    directionalLight.castShadow = true;
-    directionalLight.shadowCameraNear = 20;
-    directionalLight.shadowCameraFar = camera.far;
-    directionalLight.shadowCameraFov = 50;
-    directionalLight.shadowMapBias = 0.3;
-    directionalLight.shadowMapDarkness = 1;
-    directionalLight.shadowMapWidth = 1024;
-    directionalLight.shadowMapHeight = 1024;
+    directionalLight.castShadow         = true;
+    directionalLight.shadowCameraNear   = 20;
+    directionalLight.shadowCameraFar    = camera.far;
+    directionalLight.shadowCameraFov    = 50;
+    directionalLight.shadowMapBias      = 0.3;
+    directionalLight.shadowMapDarkness  = 1;
+    directionalLight.shadowMapWidth     = 1024;
+    directionalLight.shadowMapHeight    = 1024;
 
     // INITIAL CANVAS
     scene.add( directionalLight );  
@@ -80,7 +81,7 @@ $(document).ready(function(){
     scene.add( camera );
     scene.add( gridHelper );
 
-    // MATERIALS
+    // OBJECTS
     var icoGeometry = new THREE.IcosahedronGeometry(10, 1),
         icoMaterial = new THREE.MeshLambertMaterial( {color: 0xff0000} );
     icosahedron     = new THREE.Mesh( icoGeometry, icoMaterial );
@@ -103,7 +104,7 @@ $(document).ready(function(){
   }
   function updatePhysics() {
       // Step the physics world
-      world.step(timeStep);
+      world.step(TIME_STEP);
       // Copy coordinates from Cannon.js to Three.js
       icosahedron.position.copy(icosahedronBody.position);
       icosahedron.quaternion.copy(icosahedronBody.quaternion);
@@ -114,5 +115,35 @@ $(document).ready(function(){
   function render() {
       renderer.render( scene, camera );
   }
-      
+
+  // PLANE CONTROLS
+  var xCount = 1.0,
+      yCount = 0.0,
+      zCount = 0.0;
+  var planeVector = new CANNON.Vec3( xCount, yCount, zCount);
+  $(this).on('keydown', function(e) {
+    var left  = 37,
+        up    = 38,
+        right = 39,
+        down  = 40;
+    
+    if(e.which === up){
+      xCount += 0.01;
+    }
+    else if(e.which === down){
+      xCount -= 0.01;
+    }
+    else if(e.which === left){
+      yCount += 0.01;
+      zCount -= 0.01;
+    }
+    else if(e.which === right){
+      yCount -= 0.01;
+      zCount += 0.01;
+    }
+
+    planeVector.set(xCount,yCount, zCount);
+    groundBody.quaternion.setFromAxisAngle(planeVector,-Math.PI/2 );
+  })
+
 });
